6b20a9cea1db877f31bca37c2611277804dcccd0
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-07-22 18:07:24 +0530

    changes

--- pages/vehicles.py
+++ pages/vehicles.py
@@ -50,7 +50,7 @@ def main():
                         elements[i].style.fontSize = '15px';  // Adjust size as needed
                         elements[i].style.padding = '10px 65px';  // Adjust padding as needed
                         elements[i].style.whiteSpace = 'nowrap';  // Ensure text is in one line
-                        elements[i].style.alignItems = 'center';
+                        elements[i].style.display = 'flex';
                         elements[i].innerHTML = `{logo_img_tag} {widget_label}  {limit_text}`;
                     }}
                 }}
