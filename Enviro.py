809001811dbea989c1948a41cd8c0ec06bbffb91
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-10 23:06:23 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -148,7 +148,7 @@ def main():
     container_html = '<div class="container-box">'
     container_html += '<h2>Choose Category</h2>'
     for cat in categories:
-        link = "/Login"
+        link = "/login"
         logo_img_tag = generate_logo_html(cat, logo_path=category_logos[cat])
         container_html += f'<a href="{link}" target="_self" class="category-button">{logo_img_tag} <span>{cat}</span></a>'
     container_html += '</div>'
