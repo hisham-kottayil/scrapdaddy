144d8a60318f8f3033a2ddad326e288de140af3e
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-11 23:11:27 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -241,7 +241,7 @@ def main():
                 flex-wrap: wrap;
             }
             .service-item {
-                background-color: #E8F1DA;
+                background-color: #DBF7F1;
                 box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
                 border-radius: 8px;
                 padding: 20px;
