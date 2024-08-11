87449532d88f24551259bd836f324df7664bed77
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-11 13:39:14 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -41,7 +41,9 @@ def main():
         st.session_state['authentication_status'] = ''
     if 'authenticator_object' not in st.session_state:
         st.session_state['authenticator_object'] = '' 
-        
+    hide_pages(
+        ["styles", "Test"]
+        )
     authentication_status = st.session_state['authentication_status']
     authenticator = st.session_state['authenticator_object']
     st.write(authentication_status)
