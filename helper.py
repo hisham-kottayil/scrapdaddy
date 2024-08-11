08b6912d709b00bb988e339462cd329fa71ebf0e
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-11 13:47:20 +0530

    changes

--- helper.py
+++ helper.py
@@ -4,11 +4,12 @@ from st_pages import Page, add_page_title, hide_pages
 
 def hide_pages_dynamically(authentication_status):
     if not authentication_status or authentication_status == '':
-        print("Here")
+        st.write("Here")
         hide_pages(
             ["Materials", "Vehicle", "Address", "Checkout", "Orders"]
             )
     else:
+        st.write('Here2')
         pass
     
 def hide_pages_extras():
