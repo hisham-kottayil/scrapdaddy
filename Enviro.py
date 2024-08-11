e746812a566b9ed350d8256dda8cc6ab28281a83
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-11 20:33:02 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -186,8 +186,8 @@ def main():
             .mission-paragraph {{
                 font-size: 20px;
                 color: #2a5d2b;
-                margin: 20px 0;
-                padding: 0 50px; /* Adds padding to the left and right */
+                margin: 200px 0;
+                padding: 0 20px; /* Adds padding to the left and right */
             }}
         </style>
     """
