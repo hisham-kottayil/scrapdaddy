f466e9ffd22149367df86fa25fcc0b737a43cded
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-11 23:55:30 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -289,7 +289,7 @@ def main():
                     <div class="hiw-item">
                         <img src="{signup_logo}" alt="Signup" class="hiw-logo">
                         <p>Signup</p>
-                        <h10>Here</h10>
+                        <h15>Here</h15>
                     </div>
                     <div class="hiw-item">
                         <img src="{materials_logo}" alt="Select Materials" class="hiw-logo">
