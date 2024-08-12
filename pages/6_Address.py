a4efc7ed8e7d019c564df8367558774ec35e7df1
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-12 17:53:17 +0530

    changes

--- pages/6_Address.py
+++ pages/6_Address.py
@@ -42,7 +42,7 @@ def main():
     contact = int(contact) if contact else ''
     # Display the entered information
     if st.button('Proceed'):
-        if contact == '' or name == '' or address is None or pincode is None:
+        if contact == '' or name == '' or address == '' or pincode == '':
             st.error("Please provide all the mandatory fields to proceed.")
         else:
             st.session_state['addess'] = f"""
