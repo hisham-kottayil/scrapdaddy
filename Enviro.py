d0a642d7ba0a9db2ddafd47dcf6eb3110578c2e3
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-12 09:12:02 +0530

    changes

--- Enviro.py
+++ Enviro.py
@@ -89,7 +89,6 @@ def main():
                             .hiw-section h7 {{
                                 color: #2a5d2b; /* Dark green */
                                 margin-left: 100px;
-                                margin-right: 50px;
                             }}
                         </style>
                         """
