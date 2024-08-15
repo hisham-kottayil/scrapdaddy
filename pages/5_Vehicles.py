8d83473aedbb8890d17b3a33021a738d64c75159
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-15 15:58:16 +0530

    changes

--- pages/5_Vehicles.py
+++ pages/5_Vehicles.py
@@ -99,7 +99,7 @@ def main():
                 for (var i = 0; i < elements.length; ++i) {{ 
                     if (elements[i].innerText.includes('{widget_label}')) {{ 
                         elements[i].style.fontSize = '15px';  // Adjust size as needed
-                        elements[i].style.padding = '10px 5px';  // Adjust padding as needed
+                        elements[i].style.padding = '2px 65px';  // Adjust padding as needed
                         elements[i].style.whiteSpace = 'nowrap';  // Ensure text is in one line
                         elements[i].style.display = 'flex';
                         elements[i].style.alignItems = 'center';
