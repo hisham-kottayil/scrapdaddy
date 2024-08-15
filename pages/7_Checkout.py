712355212f0754c2c73508cc7ff846233babb030
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-15 18:30:12 +0530

    changes

--- pages/7_Checkout.py
+++ pages/7_Checkout.py
@@ -58,7 +58,7 @@ def main():
         if st.button('Schedule'):
             if 'selected_vehicle' not in st.session_state.keys():
                 st.error("Please choose vehicle to proceed.")
-            if 'address' not in st.session_state.keys():
+            if 'customer_details' not in st.session_state.keys():
                 st.error("Please add address to proceed.")
             if 'date_chosen' not in st.session_state.keys():
                 st.error("Please choose pickup date to proceed.")
