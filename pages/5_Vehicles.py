a1d733419c0e26f0ae8514d2586c1dc9659ba053
hisham mohammed <hisham.mohammed@impactanalytics.co>
2024-08-15 16:13:30 +0530

    changes

--- pages/5_Vehicles.py
+++ pages/5_Vehicles.py
@@ -123,12 +123,11 @@ def main():
                 {vehicle} <span style='color: grey;'>(max: {max_limit} kgs)</span>
             </button>
         """
-        selected_vehicle = st.button(button_html, key=f"{vehicle}_button")
-        # with cols[i % 2]:
-        #     selected_vehicle = st.button(button_html, key=f"{vehicle}_button")
-        #     if selected_vehicle:
-        #         selected = {}
-        #         selected['vehicle'] = vehicle
+        with cols[i % 2]:
+            selected_vehicle = st.button(button_html, key=f"{vehicle}_button")
+            if selected_vehicle:
+                selected = {}
+                selected['vehicle'] = vehicle
     if selected:
         st.session_state['selected_vehicle'] = selected["vehicle"]
         try:
